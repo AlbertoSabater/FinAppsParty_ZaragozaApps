fbae323198e75e6cf7a3385d9591a6a316bdeffc
SergioLazaro <sergiozgz1994@gmail.com>
2015-11-06 18:29:20 +0100

    Commit sense HAT python example

---
+++ SetImages.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys
+import time
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+def redlights():
+	for i in range(8):
+		for j in range(8):
+			sense.set_pixel(i,j,[255,0,0])
+	return
+
+def yellowlights():
+	for i in range(8):
+		for j in range(8):
+			sense.set_pixel(i,j,[255,255,0])
+	return
+
+def greenlights():
+	for i in range(8):
+			for j in range(8):
+				sense.set_pixel(i,j,[0,255,0])
+	return
+
+while True:
+	greenlights()
+	time.sleep(3)
+	yellowlights()
+	time.sleep(3)
+	redlights()
+	time.sleep(3)
